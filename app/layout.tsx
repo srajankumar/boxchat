8a691e9d4a405288d9b3cf5eca769332dba9c977
srajan <kumarsrajan02@gmail.com>
2024-04-20 16:48:19 +0530

    metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,59 @@
 import { GeistSans } from "geist/font/sans";
 import "./globals.css";
 
-const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
+import NextTopLoader from "nextjs-toploader";
+
+const defaultUrl = process.env.NEXT_PUBLIC_VERCEL_URL
+  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
   : "http://localhost:3000";
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Box Chat",
-  description: "Chat Anonymously Inside the Box",
+  title: {
+    default: "Box Chat",
+    template: "%s | Chat Anonymously Inside the Box.",
+  },
+  description: "Chat Anonymously Inside the Box.",
+  keywords: [
+    "boxchat",
+    "box chat",
+    "Supabase realtime chat",
+    "supabase chat",
+    "Anonymous chat",
+    "anonymous",
+    "anonym",
+    "chat",
+    "chat friends",
+    "peer",
+    "group",
+  ],
+  creator: "srajankumar",
+  openGraph: {
+    title: "Box Chat",
+    description: "Chat Anonymously Inside the Box.",
+    url: defaultUrl,
+    siteName: "Box Chat",
+    images: [
+      {
+        url: `${defaultUrl}/og.png`,
+        width: 1200,
+        height: 630,
+        alt: "Box Chat",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Box Chat",
+    description: "Chat Anonymously Inside the Box.",
+    creator: "@kumarsrajan02",
+    images: [`${defaultUrl}/og.png`],
+  },
+  icons: {
+    icon: "/favicons/favicon.ico",
+  },
 };
 
 export default function RootLayout({
@@ -18,7 +63,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={GeistSans.className}>
-      <body>{children}</body>
+      <body>
+        <NextTopLoader color="#000000" shadow={false} showSpinner={false} />
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
